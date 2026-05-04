e978a895bd252ac2139ff4b37a5c76c866fc090b
Ghadeer Fallatah <ghadeer1781@gmail.com>
2026-05-04 11:13:58 +0300

    inital project files

---
+++ Smack/Smack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smack
+//
+//  Created by Ghadeer Fallatah on 17/11/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smack/Smack/SmackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmackApp.swift
+//  Smack
+//
+//  Created by Ghadeer Fallatah on 17/11/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct SmackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
