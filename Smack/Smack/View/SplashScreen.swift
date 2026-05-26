4b1b648204e22e72aac9ca497c17c8a99f26ece0
Nouf Alshawoosh <Noufshawoosh@gmail.com>
2026-05-26 04:28:46 +0300

    Changed the edployment target in settings

---
+++ Smack/Smack/View/SplashScreen.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreen.swift
+//  Smack
+//
+//  Created by Nouf Alshawoosh on 26/05/2026.
+//
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
