52c274146e465081237581f4a86ee5b800c71f30
Nouf Alshawoosh <Noufshawoosh@gmail.com>
2026-05-28 17:03:04 +0300

    .

---
+++ Smack/Smack/View/PlayerOrVoterScreen.swift
@@ -0,0 +1,88 @@
+//
+//  PlayerOrVoterScreen.swift
+//  Smack
+//
+//  Created by Nouf Alshawoosh on 28/05/2026.
+//
+import SwiftUI
+
+struct PlayerOrVoterScreen: View {
+    var body: some View {
+        
+        ZStack {
+            Color(.blue).ignoresSafeArea()
+            
+            GeometryReader { geo in
+                
+                VStack {
+                    TextTitle(text: "حدد موقعك!", fontName: "Lalezar-Regular", size: geo.size.width * 0.11, strokeWidth: 2)
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        ZStack{
+                            ButtonView(width: geo.size.width * 0.8, height: geo.size.width * 0.28)
+                            PlayerOrVoterCardDesign(width: geo.size.width * 0.8, emoji: "🤾🏻", title: "لاعب", subTitle: "فاجئهم بشطحاتك!")
+                            
+                        }
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        ZStack{
+                            ButtonView(width: geo.size.width * 0.8, height: geo.size.width * 0.28)
+                            PlayerOrVoterCardDesign(width: geo.size.width * 0.8, emoji: "🧑🏻‍⚖️", title: "مصوّت", subTitle: "ايش الشطحة الرهيبة")
+                            
+                        }
+                    }
+                    
+                }.frame(width: geo.size.width, height: geo.size.height)
+                
+            } // end of GeometryReader
+            
+        } // end of ZStack
+        
+    }
+}
+
+struct PlayerOrVoterCardDesign: View {
+    var width: CGFloat  // just width, not full size
+    var emoji: String
+    var title: String
+    var subTitle: String
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            // ======= text =======
+            VStack(alignment: .trailing, spacing: 0) {
+                Text(title)
+                    .font(.custom("Lalezar-Regular", size: width * 0.1))
+                    .foregroundStyle(.black)
+                
+                Text(subTitle)
+                    .font(.custom("Tajawal-Bold", size: width * 0.05))
+                    .foregroundStyle(.black)
+            }
+            
+            Spacer().frame(width: width * 0.05)
+            // ======= circle ======
+            ZStack {
+                Circle()
+                    .frame(width: width * 0.2, height: width * 0.2)
+                    .foregroundStyle(.black)
+                Text(emoji)
+                    .font(.system(size: width * 0.1))
+            }
+            
+            Spacer().frame(width: width * 0.07)
+        }
+        .frame(width: width)
+    }
+}
+
+#Preview {
+    PlayerOrVoterScreen()
+}
